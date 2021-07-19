926f0ff56c56155f5a9b2a86e5c746a7fced4983
Bhowmik-puja <bhowmikpuja27@gmail.com>
2021-07-20 02:00:42 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <title>Health Care</title>
+</head>
+
+<body>
+    <section class="header">
+        <nav>
+            <a href="index.php"><span class="website-name">Health Care</span></a>
+            
+            <div class="navbar">
+                <ul>
+                <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Doctors</a>
+                    </li>
+                    <li>
+                        <a href="">Blood Bank</a>
+                    </li>
+                    <li>
+                        <a href="">Appointment</a>
+                    </li>
+                    <li>
+                        <a href="">SignIn</a>
+                    </li>
+                    <li>
+                        <a href="">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+<br>
+    <section class="doctors">
+        <h1>Doctors</h1>
+        <br>
+
+        <div class="box-1">
+            <div>
+                <img src="images/dummy.jpg" alt="">
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore mollitia obcaecati et tenetur veritatis laudantium sint error tempore ab possimus harum, nemo placeat molestias nostrum sunt odit blanditiis doloremque odio?</p>
+            </div>
+        </div>
+        
+        <div class="box-1">
+            <div>
+                <img src="images/dummy.jpg" alt="">
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore mollitia obcaecati et tenetur veritatis laudantium sint error tempore ab possimus harum, nemo placeat molestias nostrum sunt odit blanditiis doloremque odio?</p>
+            </div>
+        </div>
+        
+        <div class="box-1">
+            <div>
+                <img src="images/dummy.jpg" alt="">
+            </div>
+            <div>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero numquam veritatis quo et cum. Dignissimos expedita, odio blanditiis reiciendis maiores commodi iusto nam obhjohuaperiam. Exhvhvhpedita, quam possimus.</p>
+                
+            </div>
+        </div>
+        
+        <div class="box-1">
+            <div>
+                <img src="images/dummy.jpg" alt="">
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore mollitia obcaecati et tenetur veritatis laudantium sint error tempore ab possimus harum, nemo placeat molestias nostrum sunt odit blanditiis doloremque odio?</p>
+            </div>
+        </div>
+    </section>
+    <section class="blogs">
+
+    </section>
+    <footer>
+        
+    </footer>
+</body>
+
+</html>
