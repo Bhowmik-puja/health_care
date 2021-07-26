cf2459d76f52453895f5757edccf95761240def7
Bhowmik-puja <bhowmikpuja27@gmail.com>
2021-07-26 18:04:32 +0600

    version 2.1

--- patientProfile.php
+++ patientProfile.php
@@ -17,9 +17,9 @@ if (!isset($_SESSION['id'])) {
     $stmt2->execute();
     $appointments = $stmt2->fetchAll();
 }
-    // echo '<pre>';
-    // var_dump($appointments);
-    // echo '</pre>';
+// echo '<pre>';
+// var_dump($appointments);
+// echo '</pre>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,74 +28,74 @@ if (!isset($_SESSION['id'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="css/doctorProfile.css">
+    <link rel="stylesheet" href="css/doctorProfile.css">
     <title>Patient Profile</title>
 </head>
 
 <body>
     <section class="myProfile">
-    <h1>My profile</h1>
-    <div class="details-box">
-        <img src="images/demoPatient.jpg" alt="">
-    </div>
+        <h1>My profile</h1>
+        <div class="details-box">
+            <img src="images/user.png" alt="">
+        </div>
 
-    <div class="details-box">
-        <div>
-            <h4>Name:<?php echo ' ' . $profile[0]['firstName'] . ' ' . $profile[0]['lastName']; ?> </h4>
-        </div>
-        <div>
-            <h4>Email:<?php echo ' ' . $profile[0]['email']; ?> </h4>
-        </div>
-        <div>
-            <h4>Contact Number: <?php echo ' ' . $profile[0]['phone']; ?> </h4>
-        </div>
-        <div>
-            <h4>Gender:<b><?php echo ' ' . $profile[0]['gender']; ?></b> </h4>
-        </div>
-        <div>
-            <h4>Blood Group: <?php echo ' ' . $profile[0]['bloodGrp']; ?></h4>
-        </div>
-        <div>
-            <h4>Date of Birth:<?php echo ' ' . $profile[0]['dob']; ?> </h4>
-        </div>
-        <div>
-            <h4>Age: <?php
-                                $bday = new DateTime($profile[0]['dob']); // Your date of birth
-                                $today = new Datetime(date('m.d.y'));
-                                $diff = $today->diff($bday);
-                                printf('%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
-                             
-                                ?> </h4>
-        </div>
-        <div>
-            <h4>Address: <?php echo ' ' . $profile[0]['address']; ?> </h4>
-        </div>
-    </div>
+        <div class="details-box">
+            <div>
+                <span>Name:</span><?php echo ' ' . $profile[0]['firstName'] . ' ' . $profile[0]['lastName']; ?>
+            </div>
+            <div>
+                <span>Email:</span><?php echo ' ' . $profile[0]['email']; ?>
+            </div>
+            <div>
+                <span>Contact Number:</span> <?php echo ' ' . $profile[0]['phone']; ?>
+            </div>
+            <div>
+                <span>Gender:</span><b><?php echo ' ' . $profile[0]['gender']; ?></b>
+            </div>
+            <div>
+                <span>Blood Group: </span><?php echo ' ' . $profile[0]['bloodGrp']; ?>
+            </div>
+            <div>
+                <span>Date of Birth:</span><?php echo ' ' . $profile[0]['dob']; ?>
+            </div>
+            <div>
+                <span>Age: </span><?php
+                                    $bday = new DateTime($profile[0]['dob']); // Your date of birth
+                                    $today = new Datetime(date('m.d.y'));
+                                    $diff = $today->diff($bday);
+                                    printf('%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
 
-    <div class="table-box">
-        <table class="table">
-            <caption>Appointment Dashboard:</caption>
-            <thead>
-                <tr>
-                    <th scope="col">Serial</th>
-                    <th scope="col">Doctor Name</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">time</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($appointments as $i => $row) : ?>
+                                    ?>
+            </div>
+            <div>
+                <span>Address: </span><?php echo ' ' . $profile[0]['address']; ?>
+            </div>
+        </div>
+
+        <div class="table-box">
+            <table class="table">
+                <caption>Appointment Dashboard:</caption>
+                <thead>
                     <tr>
-                        <th><?PHP echo $i + 1; ?></th>
-                        <td><?php echo $row['doctorName'] ?></td>
-                        <td><?php echo $row['date'] ?></td>
-                        <td><?php echo $row['time'] ?></td>
+                        <th scope="col">Serial</th>
+                        <th scope="col">Doctor Name</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">time</th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
- </section>
+                </thead>
+                <tbody>
+                    <?php foreach ($appointments as $i => $row) : ?>
+                        <tr>
+                            <th><?PHP echo $i + 1; ?></th>
+                            <td><?php echo $row['doctorName'] ?></td>
+                            <td><?php echo $row['date'] ?></td>
+                            <td><?php echo $row['time'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </body>
 
 </html>
